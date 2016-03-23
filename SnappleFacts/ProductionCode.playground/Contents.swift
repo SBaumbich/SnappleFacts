02b3af67be2d5b041fa83a85c30ee37b83d7d232
Scott Baumbich <sbaumbich777@gmail.com>
2016-03-23 15:24:41 -0500

    Initial Commit

---
+++ SnappleFacts/ProductionCode.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let factsArray = [
+    "Animals that lay eggs don't have belly buttons.",
+    "Beavers can hold their breath for 45 minutes under water.",
+    "Slugs have four noses.",
+    "Mosquitoes are attracted to people who just ate bananas.",
+    "Emus and kangaroos cannot walk backward.",
+    "Alaska has the highest percentage of people who walk to work.",
+    "The only food that does not spoil is honey.",
+    "On average a human will spend up to 2 weeks kissing in his/her lifetime.",
+    "It is possible to lead a cow up stairs but not down.",
+    "Frogs cannot swallow with their eyes open.",
+    "Elephants are capable of swimming 20 miles per day.",
+    "Cats can hear ultrasound.",
+    "The average smell weighs 760 nanograms.",
+    "Pigs can get sunburn.",
+    "No word in the English language rhymes with month.",
+    "About 18 percent of animal owners share their bed with their pet.",
+    "Oysters can change from one gender to another and back again.",
+    "A ten gallon hat holds less than one gallon of liquid.",
+    "Lizards communicate by doing push-ups.",
+    "You burn about 20 calories per hour chewing gum.",
+    "Bamboo makes up 99 percent of a panda's diet.",
+    "Honeybees navigate by using the sun as a compass."
+]
+
+let colorsArray = [
+    UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0), //teal color
+    UIColor(red: 222/255.0, green: 171/255.0, blue: 66/255.0, alpha: 1.0), //yellow color
+    UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0), //red color
+    UIColor(red: 239/255.0, green: 130/255.0, blue: 100/255.0, alpha: 1.0), //orange color
+    UIColor(red: 77/255.0, green: 75/255.0, blue: 82/255.0, alpha: 1.0), //dark color
+    UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0), //purple color
+    UIColor(red: 85/255.0, green: 176/255.0, blue: 112/255.0, alpha: 1.0), //green color
+]
